9c0c8190efbbbb2bb8fc127683f172938c6ffe93
Sven Bendel <ubuntudroid@gmail.com>
2017-12-12 17:30:42 +0100

    SNH-1745 Add unit suffix to constant names

--- loadingindicator/src/main/java/io/stanwood/framework/loadingindicator/LoadingIndicatorView.kt
+++ loadingindicator/src/main/java/io/stanwood/framework/loadingindicator/LoadingIndicatorView.kt
@@ -4,8 +4,9 @@ import android.content.Context
 import android.util.AttributeSet
 import android.widget.LinearLayout
 
-private const val MIN_SHOW_TIME = 500
-private const val MIN_DELAY = 200
+private const val MIN_SHOW_TIME_MS = 500
+private const val MIN_DELAY_MS = 200
+private const val ANIM_DURATION_MS = 150L
 
 /**
  * The view for the loading indicator.
@@ -40,10 +41,10 @@ class LoadingIndicatorView @JvmOverloads constructor(context: Context,
 
     private fun animateState(expand: Boolean) {
         if (expand && !isExpanded) {
-            animate().scaleY(1f).setDuration(150).start()
+            animate().scaleY(1f).setDuration(ANIM_DURATION_MS).start()
             isExpanded = true
         } else if (!expand && isExpanded) {
-            animate().scaleY(0f).setDuration(150).start()
+            animate().scaleY(0f).setDuration(ANIM_DURATION_MS).start()
             isExpanded = false
         }
     }
@@ -59,15 +60,15 @@ class LoadingIndicatorView @JvmOverloads constructor(context: Context,
     }
 
     /**
-     * Animates the loading indicator in/out from/to the top if after [MIN_DELAY] for at least
-     * [MIN_SHOW_TIME].
+     * Animates the loading indicator in/out from/to the top if after [MIN_DELAY_MS] for at least
+     * [MIN_SHOW_TIME_MS].
      *
      * To avoid flickering a few restrictions have been set in place:
      *
      * If the view is set to be expanded and within the delay the method is called again with
      * `expanded = false` the nothing happens.
      *
-     * If the view is set to be hidden within the `MIN_DELAY + MIN_SHOW_TIME` after it has been set
+     * If the view is set to be hidden within the `MIN_DELAY_MS + MIN_SHOW_TIME_MS` after it has been set
      * to be expanded the indicator will stay expanded until this time frame has passed.
      *
      * @param expanded if `true` the loading indicator will be animated in, if `false` it will be
@@ -79,18 +80,18 @@ class LoadingIndicatorView @JvmOverloads constructor(context: Context,
             isDismissed = false
             removeCallbacks(delayedHide)
             if (!isPostedShow) {
-                postDelayed(delayedShow, MIN_DELAY.toLong())
+                postDelayed(delayedShow, MIN_DELAY_MS.toLong())
                 isPostedShow = true
             }
         } else {
             isDismissed = true
             removeCallbacks(delayedShow)
             val diff = System.currentTimeMillis() - startTime
-            if (diff >= MIN_SHOW_TIME || startTime == -1L) {
+            if (diff >= MIN_SHOW_TIME_MS || startTime == -1L) {
                 animateState(expand = false)
             } else {
                 if (!isPostedHide) {
-                    postDelayed(delayedHide, MIN_SHOW_TIME - diff)
+                    postDelayed(delayedHide, MIN_SHOW_TIME_MS - diff)
                     isPostedHide = true
                 }
             }
